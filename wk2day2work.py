edd1751a231641db5f76437577944bdaab89ae91
Pipperonni <blueas2019@gmail.com>
2023-01-31 18:17:17 -0600

    added a sort opption

--- wk2day2work.py
+++ wk2day2work.py
@@ -4,18 +4,20 @@
 # For example: Say your input parameter to the function is [1,11,14,5,8,9]...Your output should [1,5,8,9]
 # Use the following list - [1,11,14,5,8,9]
 
-l_1 = [1,11,14,5,8,9]
+l_1 = [1,11,14,5,8,9,2,23,7]
 
-def less_then_ten(alist):
+def less_then_ten(alist,sort_yes_no=""):
     new_alist = []
     for number in alist:
         if number <= 9:
             new_alist.append(number)
         else:
             continue
+    if sort_yes_no == "yes":
+        new_alist.sort()
     return new_alist            
 
-
+print(less_then_ten(l_1))
 # Exercise 2
 # Write a function that takes in two lists and returns the two lists merged together and sorted
 # Hint: You can use the .sort() method
